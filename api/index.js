e94b0f227334ab3d686a794bb7e00b2f1dfd72fc
Lovisha Goyal <lavishagoyal753@gmail.com>
2026-03-17 00:19:08 +0530

    file updated

--- api/index.js
+++ api/index.js
@@ -67,6 +67,11 @@ const corsOptions = {
     credentials: true
 };
 app.use(cors(corsOptions));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,()=>{
+    connectDB();
+    console.log(`Server running at port ${PORT}`);
+})
 
 app.get("/", (req, res) => {
   res.send("Backend is running 🚀");
