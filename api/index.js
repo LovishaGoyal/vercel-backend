432d1378b0827d19a92a5242e9a6b79b1d3330ab
Lovisha Goyal <lavishagoyal753@gmail.com>
2026-03-16 15:38:21 +0530

    update files

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
+
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
@@ -32,7 +33,7 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-
+connectDB();
 
 // app.listen(PORT,()=>{
 //     connectDB();
